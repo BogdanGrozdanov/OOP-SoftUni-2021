8ad7232d4160a515a5d9f098f41625e6acbc6f45
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-01 17:17:31 +0000

    Create Person.cs

---
+++ 02.ENCAPSULATION/Lab/01.Person/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public Person(string firstName, string lastname, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastname;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+
+
+    }
+}
