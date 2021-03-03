ea71f5b388ed1e2c85461d8bd2cfed453b75a762
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-03 09:56:41 +0000

    Create Person.cs

---
+++ 02.ENCAPSULATION/Lab/03.PersonsInfo/Person.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastname, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastname;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} gets {this.Salary:f2} leva.";
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (ValidateName(value))
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (ValidateName(value))
+                {
+                    lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            set
+            {
+                if (value < 460)
+                {
+                    salary = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+            }
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+
+            if (this.Age < 30)
+            {
+                this.Salary = this.Salary * percentage / 200;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+        }
+        private bool ValidateName(string name)
+        {
+            return name?.Length > 2;
+        }
+
+
+
+    }
+}
