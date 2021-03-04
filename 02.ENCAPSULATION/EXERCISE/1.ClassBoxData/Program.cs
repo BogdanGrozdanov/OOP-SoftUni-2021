55f267278039b6c9d9862eeb6ca1a8e7a5a9af27
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-04 22:48:13 +0000

    Create Program.cs

---
+++ 02.ENCAPSULATION/EXERCISE/1.ClassBoxData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            try
+            {
+                Box box = new Box(lenght, width, height);
+
+                double surfaceArea = box.SurfaceArea(box);
+                double lateralSurfaceArea = box.LateralSurfaceArea(box);
+                double volume = box.Volume(box);
+
+                Console.WriteLine($"Surface Area - {surfaceArea:f2}");
+                Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+                Console.WriteLine($"Volume - {volume:f2}");
+            }
+            catch ( ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+    }
+}
