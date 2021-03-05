78d886e7811956dc729df5dc5acf3477e76c1b08
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-05 13:21:08 +0000

    Create Program.cs

---
+++ 02.ENCAPSULATION/EXERCISE/2.AnimalFarm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AnimalFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine($"Chicken {chicken.Name} (age {chicken.Age}) can produce {chicken.ProductPerDay()} eggs per day.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+           
+           
+        }
+    }
+    
+}
