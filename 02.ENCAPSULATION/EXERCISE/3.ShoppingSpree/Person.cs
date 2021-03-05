df91a8cbf16568fd8d4f7619c1d4e76adecc3346
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-05 18:59:26 +0000

    Create Person.cs

---
+++ 02.ENCAPSULATION/EXERCISE/3.ShoppingSpree/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3.ShoppingSpree
+{
+    public class Person
+    {
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.products = new List<Product>();
+        }
+        private string name;
+        private decimal money;
+        private List<Product> products;
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                else
+                {
+                    this.name = value;
+                }
+            }
+        }
+        public decimal Money
+        {
+            get { return this.money; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+        public void AddProduct(Product product)
+        {
+            if (product.Cost > this.Money)
+            {
+                throw new InvalidOperationException($"{this.Name} can't afford {product.Name}");
+            }
+            this.products.Add(product);
+            this.Money -= product.Cost;
+        }
+        public override string ToString()
+        {
+            if (this.products.Count == 0)
+            {
+                return $"{this.Name} - Nothing bought";
+            }
+            return $"{this.Name} - {string.Join(", ", this.products.Select(p => p.Name))}";
+        }
+
+
+    }
+}
