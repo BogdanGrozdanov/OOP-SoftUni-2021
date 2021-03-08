ad3a8ae7083efc520a86b43af96c94b00d73c2c5
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-08 22:40:34 +0000

    Create Circle.cs

---
+++ 03.InterfacesAndAbstraction/Lab/01.Shapes/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private double radius;
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public void Draw()
+        {
+            double rIn = this.Radius - 0.4;
+            double rOut = this.Radius + 0.4;
+            for (double y = this.Radius; y >= -this.Radius; --y)
+            {
+                for (double x = -this.Radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
