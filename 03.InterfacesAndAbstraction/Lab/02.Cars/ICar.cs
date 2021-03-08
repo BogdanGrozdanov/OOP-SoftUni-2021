606abbb1879b2aa8687a67e56828b59db8e49510
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-08 22:40:22 +0000

    Create ICar.cs

---
+++ 03.InterfacesAndAbstraction/Lab/02.Cars/ICar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    interface ICar
+    {
+        public string Model { get; }
+        public string Color { get; }
+
+        public string Start();
+        public string Stop();
+    }
+}
+
