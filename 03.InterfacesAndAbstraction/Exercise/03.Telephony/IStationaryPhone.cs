b15ba8b94879ecd7074b87f98e341dd82869a45d
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-09 13:48:59 +0000

    Create IStationaryPhone.cs

---
+++ 03.InterfacesAndAbstraction/Exercise/03.Telephony/IStationaryPhone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Telephony
+{
+    public interface IStationaryPhone
+    {
+        public string PhoneNumber { get; }
+
+        string MakeCall();
+    }
+}
