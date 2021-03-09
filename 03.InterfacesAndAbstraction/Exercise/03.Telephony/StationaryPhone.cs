5e1b705c84080af8c3417570f69cd0d330b126cc
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-09 13:49:08 +0000

    Create StationaryPhone.cs

---
+++ 03.InterfacesAndAbstraction/Exercise/03.Telephony/StationaryPhone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Telephony
+{
+    public class StationaryPhone : IStationaryPhone
+    {
+        private string phoneNumber;
+        public string PhoneNumber
+        {
+            get { return this.phoneNumber; }
+            set
+            {
+                ThrowIfNumberIsInvalid(value);
+                this.phoneNumber = value;
+            }
+        }
+        public StationaryPhone(string number)
+        {
+            this.PhoneNumber = number;
+        }
+
+        private void ThrowIfNumberIsInvalid(string digits)
+        {
+            var charDigits = digits.ToCharArray();
+            for (int i = 0; i < charDigits.Length; i++)
+            {
+                if (!Char.IsDigit(charDigits[i]))
+                {
+                    throw new ArgumentException("Invalid number!");
+                }
+            }
+        }
+
+        public string MakeCall()
+        {
+            return $"Dialing... {this.PhoneNumber}";
+        }
+    }
+}
