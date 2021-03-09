0e6c36d8e96ab906dbd6ffedfe7708d1420343bc
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-09 13:49:01 +0000

    Create ISmartphone.cs

---
+++ 03.InterfacesAndAbstraction/Exercise/03.Telephony/ISmartphone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Telephony
+{
+    public interface ISmartphone : IStationaryPhone
+    {
+        public string URL { get; }
+        public string Browsing();
+    }
+}
