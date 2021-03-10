8e1d3dda425b21b7a866d334217e942e52ac47ba
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-10 18:39:27 +0000

    Create Citizen.cs

---
+++ 03.InterfacesAndAbstraction/Exercise/05.BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.BirthdayCelebrations
+{
+    public class Citizen : IBirthable
+    {
+        public Citizen(string name, string age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+        public string Name { get; private set; }
+
+        public string Age { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
