07bf47f802e90097ddcdaa81e4e8667c76a618ad
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-10 11:05:41 +0000

    Create Program.cs

---
+++ 03.InterfacesAndAbstraction/Exercise/04.BorderControl/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.BorderControl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<IIds> id = new List<IIds>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "End")
+                {
+                    break;
+                }
+
+                if (input.Length == 2)
+                {
+                    id.Add(new Robot(input[0],input[1]));
+                }
+                else
+                {
+                    id.Add(new Citizens(input[0],input[1],input[2]));
+                }
+            }
+            var failId = Console.ReadLine();
+            var filtredId = id.Where(p=>p.Id.EndsWith(failId)).ToList();
+            foreach (var item in filtredId)
+            {
+                Console.WriteLine(item.Id);
+            }
+        }
+    }
+}
