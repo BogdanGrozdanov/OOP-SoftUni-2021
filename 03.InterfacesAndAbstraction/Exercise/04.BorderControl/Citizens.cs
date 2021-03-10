989fbdd3c95ec3a7017f3fb4daf06e931902e3c2
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-10 11:05:35 +0000

    Create Citizens.cs

---
+++ 03.InterfacesAndAbstraction/Exercise/04.BorderControl/Citizens.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.BorderControl
+{
+    public class Citizens : IIds
+    {
+        public Citizens(string name, string age, string id)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+        }
+        public string Name { get; set; }
+
+        public string Age { get; set; }
+
+        public string Id { get; set; }
+    }
+}
