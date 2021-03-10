eb86e07fd030bda1e2eaed5393a3cb474e0bf705
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-10 18:39:35 +0000

    Create IBirthable.cs

---
+++ 03.InterfacesAndAbstraction/Exercise/05.BirthdayCelebrations/IBirthable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.BirthdayCelebrations
+{
+    interface IBirthable
+    {
+        string Name { get; }
+        string Birthdate { get; }
+    }
+}
