89ef4f5fe1a34ffc7777e8003462fc6469a3ff96
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-11 21:56:49 +0000

    Create Animal.cs

---
+++ 04Polymorphism/Lab/02.Animal/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private readonly string name;
+        private readonly string favouriteFood;
+        public Animal(string name, string food)
+        {
+            this.name = name;
+            this.favouriteFood = food;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {name} and my fovourite food is {favouriteFood}";
+        }
+    }
+}
