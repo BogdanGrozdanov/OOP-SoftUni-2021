8de422056b177807fb9058ae0719b1943a96d5de
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-11 21:56:38 +0000

    Create Dog.cs

---
+++ 04Polymorphism/Lab/02.Animal/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, string food) : base(name, food)
+        {
+
+        }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "DJAAF";
+        }
+
+    }
+}
