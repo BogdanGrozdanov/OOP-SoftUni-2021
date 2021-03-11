bcc5f1eab648f05923cbe667722b66d5f6087dc9
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-11 13:42:13 +0000

    Create Citizen.cs

---
+++ 03.InterfacesAndAbstraction/Exercise/06.FoodShortage/Citizen.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.FoodShortage
+{
+    public class Citizen : IBuyer
+    {
+        public Citizen(string name, string age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+            this.Food = 0;
+        }
+        public string Name { get; private set; }
+
+        public string Age { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+
+        public int Food { get; private set; }
+
+        public int BuyFood()
+        {
+            return this.Food += 10;
+        }
+    }
+}
