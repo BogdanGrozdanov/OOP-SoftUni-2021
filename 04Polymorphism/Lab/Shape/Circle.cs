4e93373da065d23d0c54e4b077653e55e9431b09
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-12 09:49:10 +0000

    Create Circle.cs

---
+++ 04Polymorphism/Lab/Shape/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private readonly double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * this.radius * this.radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Math.PI * this.radius * 2;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + GetType().Name;
+        }
+
+
+    }
+}
