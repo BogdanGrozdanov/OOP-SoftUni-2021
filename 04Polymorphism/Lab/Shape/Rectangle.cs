3e940fd7a25d145166ddf48103c1cc373659c856
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-12 09:49:15 +0000

    Create Rectangle.cs

---
+++ 04Polymorphism/Lab/Shape/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+
+        public override double CalculateArea()
+        {
+            return this.height * this.widht;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * height + 2 * widht;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + "Rectangle";
+        }
+        private readonly double height;
+
+        private readonly double widht;
+
+        public Rectangle(double height, double widht)
+        {
+            this.height = height;
+            this.widht = widht;
+        }
+
+
+    }
+}
