fb51b418e9606f607f5afde56a0d3d700d940fe9
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-13 11:48:22 +0000

    Create Truck.cs

---
+++ 04Polymorphism/Exercise/Vehicles/Truck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Truck : Vehicle
+    {
+        private const double summerConsump = 1.6;
+
+        public Truck(double fuelQuantity, double fuelConsump) : base(fuelQuantity, fuelConsump, summerConsump)
+        {
+        }
+         
+
+        public override void Refueling(double refillFuel)
+        {
+            base.Refueling(refillFuel * 0.95);
+        }
+    }
+}
