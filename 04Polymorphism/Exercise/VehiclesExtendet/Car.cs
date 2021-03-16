779a741fecb17cc1b1ae28a7cac206a70a064648
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-16 09:59:03 +0000

    Create Car.cs

---
+++ 04Polymorphism/Exercise/VehiclesExtendet/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Car : Vehicle
+    {
+        private const double CarSummerConsump = 0.9;
+
+        public Car(double fuelQuantity, double fuelConsump, double tankCapacity) : base(fuelQuantity, fuelConsump, CarSummerConsump, tankCapacity)
+        {
+        }
+    }
+}
