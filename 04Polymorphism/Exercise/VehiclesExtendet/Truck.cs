17326bfdd721e5ae7df2f4f500b8cdb27fa9a803
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-16 09:59:11 +0000

    Create Truck.cs

---
+++ 04Polymorphism/Exercise/VehiclesExtendet/Truck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Truck : Vehicle
+    {
+        private const double TrucksummerConsump = 1.6;
+
+        public Truck(double fuelQuantity, double fuelConsump, double tankCapacity) : base(fuelQuantity, fuelConsump, TrucksummerConsump, tankCapacity)
+        {
+        }
+
+        public override void Refueling(double refillFuel)
+        {
+            var totalFuel = FuelQuantity + refillFuel;
+            if (refillFuel <= 0)
+            {
+                throw new InvalidOperationException("Fuel must be a positive number");
+            }
+            if (TankCapacity < totalFuel)
+            {
+                throw new InvalidOperationException($"Cannot fit {refillFuel} fuel in the tank");
+            }
+            FuelQuantity += refillFuel * 0.95;
+        }
+    }
+}
