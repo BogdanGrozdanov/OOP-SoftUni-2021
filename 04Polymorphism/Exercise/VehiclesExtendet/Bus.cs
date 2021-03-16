feaab200c82aa450b1e329425d4ecc96b73e6160
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-16 09:59:01 +0000

    Create Bus.cs

---
+++ 04Polymorphism/Exercise/VehiclesExtendet/Bus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    internal class Bus : Vehicle
+    {
+        private const double withPeopleConsump = 1.4;
+
+        public Bus(double fuelQuantity, double fuelConsump, double tankCapacity) : base(fuelQuantity, fuelConsump, withPeopleConsump, tankCapacity)
+        {
+        }
+    }
+}
