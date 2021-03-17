c0b68aff5f4922ee09e8664ca95c6acf59a88760
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-17 09:22:45 +0000

    Create Warrior.cs

---
+++ 04Polymorphism/Exercise/Raiding/Warrior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Warrior : BaseHero
+    {
+        private const int WariorPower = 100;
+
+        public Warrior(string name) : base(name, WariorPower)
+        {
+        }
+        public override string CastAbility()
+        {
+            return $"{GetType().Name} - {Name} hit for {WariorPower} damage";
+        }
+    }
+}
