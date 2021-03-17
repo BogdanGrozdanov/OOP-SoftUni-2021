046a69c7036f49b45d2b2edf62c5f1071003ea22
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-17 09:22:27 +0000

    Create BaseHero.cs

---
+++ 04Polymorphism/Exercise/Raiding/BaseHero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        public string Name { get; private set; }
+        public int Power { get; private set; }
+
+        public BaseHero(string name, int power)
+        {
+            this.Name = name;
+            this.Power = power;
+        }
+
+        public virtual string CastAbility()
+        {
+            return "string.empty";
+        }
+
+    }
+}
