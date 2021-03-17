42d2772082e7696bedab4e65de5a7ea057a05e92
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-17 09:22:35 +0000

    Create Rogue.cs

---
+++ 04Polymorphism/Exercise/Raiding/Rogue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Rogue : BaseHero
+    {
+        private const int RoguePower = 80;
+
+        public Rogue(string name) : base(name, RoguePower)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{GetType().Name} - {Name} hit for {RoguePower} damage";
+        }
+    }
+}
