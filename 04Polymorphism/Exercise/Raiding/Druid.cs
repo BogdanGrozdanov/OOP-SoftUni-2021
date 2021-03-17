4e2cc42ed19e4fb0e92eb4f7e3b73265d918b897
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-17 09:22:30 +0000

    Create Druid.cs

---
+++ 04Polymorphism/Exercise/Raiding/Druid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Druid : BaseHero
+    {
+        private const int DruidPower = 80;
+        public Druid(string name) : base(name, DruidPower)
+        {
+        }
+        public override string CastAbility()
+        {
+            return $"{GetType().Name} - {Name} healed for {DruidPower}";
+        }
+
+    }
+}
