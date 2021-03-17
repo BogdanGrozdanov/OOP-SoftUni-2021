082763392f3dd4cfbd05fcfa599a81d4cc0405a9
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-17 09:22:43 +0000

    Create StartUp.cs

---
+++ 04Polymorphism/Exercise/Raiding/StartUp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Raiding
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<BaseHero> allHero = new List<BaseHero>();
+            int n = int.Parse(Console.ReadLine());
+
+            while (allHero.Count < n)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+                BaseHero hero = CreatHero(type, name);
+                if (hero == null)
+                {
+                    Console.WriteLine("Invalid hero!");
+                    continue;
+                }
+                allHero.Add(hero);
+            }
+
+
+
+            int bossHealt = int.Parse(Console.ReadLine());
+            foreach (BaseHero hero in allHero)
+            {
+                Console.WriteLine(hero.CastAbility());
+                bossHealt -= hero.Power;
+            }
+            if (bossHealt <= 0)
+            {
+                Console.WriteLine("Victory!");
+            }
+            else
+            {
+                Console.WriteLine("Defeat...");
+            }
+        }
+
+        private static BaseHero CreatHero(string type, string name)
+        {
+            BaseHero hero = null;
+            if (type == nameof(Druid))
+            {
+                hero = new Druid(name);
+            }
+            else if (type == nameof(Paladin))
+            {
+                hero = new Paladin(name);
+            }
+            else if (type == nameof(Rogue))
+            {
+                hero = new Rogue(name);
+            }
+            else if (type == nameof(Warrior))
+            {
+                hero = new Warrior(name);
+            }
+
+            return hero;
+
+        }
+    }
+}
