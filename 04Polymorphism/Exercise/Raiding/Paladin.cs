ec04627aa41b120db9ed927dfda6d53e1ff1e2e1
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-17 09:22:33 +0000

    Create Paladin.cs

---
+++ 04Polymorphism/Exercise/Raiding/Paladin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Paladin : BaseHero
+    {
+        private const int PaladinPower = 100;
+        public Paladin(string name) : base(name, PaladinPower)
+        {
+        }
+        public override string CastAbility()
+        {
+            return $"{GetType().Name} - {Name} healed for {PaladinPower}";
+
+        }
+    }
+}
