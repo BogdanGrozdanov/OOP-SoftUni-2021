1d924a09cf4e671d77fc746dfbf09262259be9ef
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 15:52:35 +0000

    Create Cat.cs

---
+++ 04Polymorphism/Exercise/WildFarm/Cat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Cat : Feline
+    {
+        private const double CatModifier = 0.30;
+
+        private static HashSet<string> allowedFood = new HashSet<string> { nameof(Meat), nameof(Vegetable) };
+
+        public Cat(string name, double weight, string livingRegion, string breed) : base(name, weight, allowedFood, CatModifier, livingRegion, breed)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+
+        
+    }
+}
