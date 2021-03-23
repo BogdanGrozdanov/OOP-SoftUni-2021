cfcc7c11af442b0f3a47a01de84d971dc12e7d0c
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 15:52:07 +0000

    Create Owl.cs

---
+++ 04Polymorphism/Exercise/WildFarm/Owl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Owl : Bird
+    {
+        private const double OwlModifier = 0.25;
+
+        private static HashSet<string> owlAllowedFood = new HashSet<string> { nameof(Meat) };
+
+        public Owl(string name, double weight, double wingSize) : base(name, weight, owlAllowedFood, OwlModifier, wingSize)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Hoot Hoot";
+        }
+    }
+}
