aaf9a95d973a92f3437e821f2ea2bc413fcb879c
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 22:01:42 +0000

    refacture

---
+++ 06.Reflection and Attributes/Lab/03.MissionPrivateImpossible/StartUp .cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            var result = spy.RevealPrivateMethods("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
