dd21a971119141425984c26ca40adfb8902730c3
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 15:52:05 +0000

    Create Hen.cs

---
+++ 04Polymorphism/Exercise/WildFarm/Hen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Hen : Bird
+    {
+        private const double HenModifier = 0.35;
+
+        private static HashSet<string> henAllowedFood = new HashSet<string>
+        {
+            nameof(Meat) ,
+            nameof(Fruit),
+            nameof(Vegetable),
+            nameof(Seeds)
+        };
+
+        public Hen(string name, double weight, double wingSize) : base(name, weight, henAllowedFood, HenModifier, wingSize)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Cluck";
+        }
+    }
+}
