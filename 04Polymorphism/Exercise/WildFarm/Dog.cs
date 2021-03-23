a7a1d66460d0000201cb1245a52aa9655022e4b1
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 15:52:38 +0000

    Create Dog.cs

---
+++ 04Polymorphism/Exercise/WildFarm/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Dog : Mammal
+    {
+        private const double DogModifier = 0.40;
+
+        private static HashSet<string> allowedFood = new HashSet<string> { nameof(Meat) };
+
+        public Dog(string name, double weight, string livingRegion) : base(name, weight, allowedFood, DogModifier, livingRegion)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Woof!";
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {Weight}, {LivingRegion}, {FoodEaten}]";
+        }
+    }
+}
