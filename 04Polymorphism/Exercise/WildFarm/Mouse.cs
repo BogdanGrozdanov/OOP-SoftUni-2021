f1af7b6f0139f7a1b213e580c6c66b1e51bc82b4
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 15:52:09 +0000

    Create Mouse.cs

---
+++ 04Polymorphism/Exercise/WildFarm/Mouse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Mouse : Mammal
+    {
+        private const double MouseModifier = 0.10;
+
+        private static HashSet<string> allowedFood = new HashSet<string>
+        {
+            nameof(Fruit),
+            nameof(Vegetable)
+        };
+        public Mouse(string name, double weight, string livingRegion) : base(name, weight, allowedFood, MouseModifier, livingRegion)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Squeak";
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {Weight}, {LivingRegion}, {FoodEaten}]";
+        }
+    }
+}
