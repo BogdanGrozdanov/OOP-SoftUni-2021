e65df26417614c0441edec168f52d3f49b9f6369
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 15:52:16 +0000

    Create Tiger.cs

---
+++ 04Polymorphism/Exercise/WildFarm/Tiger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Tiger : Feline
+    {
+        private const double TigerModifier = 1.0;
+
+        private static HashSet<string> allowedFood = new HashSet<string> { nameof(Meat) };
+
+        public Tiger(string name, double weight, string livingRegion, string breed) : base(name, weight, allowedFood, TigerModifier, livingRegion, breed)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "ROAR!!!";
+        }
+    }
+}
