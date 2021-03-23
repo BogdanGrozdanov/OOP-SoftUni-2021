09c9f84120bcd3ae456cf8ae62d2f5619b39bda3
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 15:52:48 +0000

    Create Feline.cs

---
+++ 04Polymorphism/Exercise/WildFarm/Feline.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public abstract class Feline : Mammal
+    {
+        public string Breed { get; private set; }
+
+        protected Feline(string name, double weight, HashSet<string> allowedFood, double weightModifier, string livingRegion, string breed) : base(name, weight, allowedFood, weightModifier, livingRegion)
+        {
+            this.Breed = breed;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {this.Breed}, {this.Weight}, {this.LivingRegion}, {FoodEaten}]";
+        }
+
+    }
+}
