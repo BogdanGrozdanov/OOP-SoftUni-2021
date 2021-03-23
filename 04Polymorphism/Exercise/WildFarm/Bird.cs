d6a29824ac92f69d4365b85c51e05342a4cf202e
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 15:52:45 +0000

    Create Bird.cs

---
+++ 04Polymorphism/Exercise/WildFarm/Bird.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public abstract class Bird : Animal
+    {
+        public double WingSize { get; private set; }
+
+        protected Bird(string name, double weight, HashSet<string> allowedFood, double weightModifier, double wingSize) : base(name, weight, allowedFood, weightModifier)
+        {
+            WingSize = wingSize;
+        }
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {WingSize}, {Weight}, {FoodEaten}]";
+        }
+    }
+}
