dd60bf2c8a8ac1e2b7a765bd244a400e4db0b54f
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-03-23 15:52:03 +0000

    Create Mammal.cs

---
+++ 04Polymorphism/Exercise/WildFarm/Mammal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public abstract class Mammal : Animal
+    {
+        public string LivingRegion { get; private set; }
+
+        protected Mammal(string name, double weight, HashSet<string> allowedFood, double weightModifier, string livingRegion) : base(name, weight, allowedFood, weightModifier)
+        {
+            this.LivingRegion = livingRegion;
+        }
+    }
+}
