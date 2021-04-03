85dab96501fef0685e84ac14ee37f5d18c3d7388
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2021-04-03 10:26:37 +0100

    Create Validator.cs

---
+++ 02.ENCAPSULATION/EXERCISE/5.FootballTeamGenerator/Validator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public static class Validator
+    {
+        public static void Throw_If_String_Is_Null_OR_Empty(string str, string exceptionMesage)
+        {
+            if (String.IsNullOrEmpty(str))
+            {
+                throw new ArgumentException(exceptionMesage);
+            }
+        }
+
+        public static void Throw_If_Number_Is_Out_Of_Range(int number, int min, int max, string exceptionMesage)
+        {
+            if (number < min || number > max)
+            {
+                throw new ArgumentException(exceptionMesage);
+            }
+        }
+    }
+
+}
